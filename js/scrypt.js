943b4be6bf51cb93995a81c05237f7a64080f9c2
Thomas Mielke <101403983+thomasmcodes@users.noreply.github.com>
2023-12-17 20:02:46 -0500

    Changed html and js
    Updated code to add "Your" in place of the name in the H1 in the case where the user doesn't provide a name.

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,7 @@
 const userName = prompt('Please Enter Your Name: ');
 
-document.getElementById('user-name').innerHTML = userName;
+if (userName === '' || userName === null){ // Gives a value of 'Your' if the user doesn't provide a name, either by subitting an empty prompt or cancalling the prompt
+    document.getElementById('user-name').innerHTML = 'Your';
+} else {
+    document.getElementById('user-name').innerHTML = userName + '\'s';
+}
